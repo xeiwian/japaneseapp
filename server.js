54571fb727b3326c3e2a84c121c3b15ce1b05afd
adamlee680 <adamlee680@gmail.com>
2020-04-09 23:58:05 +0800

    changed port and added a procfile for heroku

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ app.use("/api", apiRoutes);
 //     res.json(customers);
 // });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
