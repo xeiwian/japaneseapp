38da4b09042b471505dac3c83c270d746c3f5aa4
adamlee680 <adamlee680@gmail.com>
2020-04-15 17:14:50 +0800

    backup commit

--- client/src/components/practice/practicePage.js
+++ client/src/components/practice/practicePage.js
@@ -102,88 +102,17 @@ class PracticePage extends Component {
     }
 
     // global variable
-    chosenwords = [
-      {
-        type: "Emotion",
-        content: "興奮した (kōfun shita) => Excited",
-        question: "興奮した (kōfun shita)",
-        correctAnswer: "Excited",
-        possibleAnswer: ["Smile", "Happy", "Excited"]
-    },
-    {
-        type: "Emotion",
-        content: "怖い (kowai) => Scared",
-        question: "怖い (kowai)",
-        correctAnswer: "Scared",
-        possibleAnswer: ["Worried", "Nervous", "Scared"]
-    },
-    {
-        type: "Emotion",
-        content: "叫ぶ (sakebu) => Shout",
-        question: "叫ぶ (sakebu)",
-        correctAnswer: "Shout",
-        possibleAnswer: ["Angry", "Shout", "Frown"]
-    },
-    {
-        type: "Family",
-        content: "兄弟 (kyōdai) => Brothers",
-        question: "兄弟 (kyōdai)",
-        correctAnswer: "Brothers",
-        possibleAnswer: ["Cousin", "Brothers", "Brother-in-law"]
-    },
-    {
-        type: "Family",
-        content: "子供 (kodomo) => Child",
-        question: "子供 (kodomo)",
-        correctAnswer: "Child",
-        possibleAnswer: ["Child", "Baby", "Children"]
-    },
-    {
-        type: "Family",
-        content: "娘 (musume) => Daughter",
-        question: "娘 (musume)",
-        correctAnswer: "Daughter",
-        possibleAnswer: ["Niece", "Daughter", "Child"]
-    },
-    {
-        type: "Food",
-        content: "朝ごはん (asa gohan) => Breakfast",
-        question: "朝ごはん (asa gohan)",
-        correctAnswer: "Breakfast",
-        possibleAnswer: ["Breakfast", "Dinner", "Lunch"]
-    },
-    {
-        type: "Food",
-        content: "コーヒー (kōhī) => Coffee",
-        question: "コーヒー (kōhī)",
-        correctAnswer: "Coffee",
-        possibleAnswer: ["Milk", "Tea", "Coffee"]
-    },
-    {
-        type: "Food",
-        content: "林檎 (ringo) => Apple",
-        question: "林檎 (ringo)",
-        correctAnswer: "Apple",
-        possibleAnswer: ["Orange", "Grapes", "Apple"]
-    },
-    {
-        type: "Food",
-        content: "林檎 (ringo) => Apple",
-        question: "林檎 (ringo)",
-        correctAnswer: "Apple",
-        possibleAnswer: ["Orange", "Grapes", "Apple"]
-    }
-    ];
+    chosenwords = [];
 
     getChosenWords = async (id) => {
         return await fetch(`api/userlogin/getchosenwords/${id}`);
     }
 
     handleChosenWords = async () => {
-        // let data = await this.getChosenWords(this.props.id);
-        // let datajson = await data.json();
-        // this.chosenwords = datajson.chosenwords;
-        // console.log('i am chosenwords data in practice', datajson, this.chosenwords);
+        let data = await this.getChosenWords(this.props.id);
+        let datajson = await data.json();
+        this.chosenwords = datajson.chosenwords;
+        console.log('i am chosenwords data in practice', datajson, this.chosenwords);
     
         this.setState({
           content: this.chosenwords[this.state.counter].content,
