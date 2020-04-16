11744132739db20391982e283a8207d2f5b477a2
adamlee680 <adamlee680@gmail.com>
2020-04-16 22:35:08 +0800

    added dbName in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const apiRoutes = require("./routes");
 const bodyParser = require("body-parser");
 var path = require('path');
 
-mongoose.connect(process.env.MONGOLAB_URL || 'mongodb://localhost:27017/appdb', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGOLAB_URL || 'mongodb://localhost:27017/appdb', {useNewUrlParser: true, useUnifiedTopology: true, dbName: 'appdb'});
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const app = express();
